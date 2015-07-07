2b702fa593f2b8ec81896d38d7eec3a671c1b8ff
PlamenMinkov <minkov.plamen@yahoo.com>
2015-07-07 14:53:47 +0300

    first final design

--- ctac.js
+++ ctac.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
     
-    $('.jq_su').click(function(){                       $('.jq2').addClass('active')
+    $('.jq_su').click(function(){                               $('.jq2').addClass('active')
       $('.jq2').removeClass('inactive')
       $('.jq1').addClass('inactive')
       $('.jq1').removeClass('active')
     });
     
-    $('.jq_si').click(function(){                       $('.jq1').addClass('active')
+    $('.jq_si').click(function(){                               $('.jq1').addClass('active')
       $('.jq1').removeClass('inactive')
       $('.jq2').addClass('inactive')
       $('.jq2').removeClass('active')
